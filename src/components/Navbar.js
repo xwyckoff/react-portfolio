4ee9460e76083596f2261b87c396f3a25ca0627f
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-06-15 00:02:50 -0400

    would probably be a good idea if I added an onclick function to the navbar buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,16 +7,16 @@ export default function Navbar({ currentPage, handlePageChange, username }) {
                 <a className="navbar-brand" href="#home" onClick={() => handlePageChange('Home')}>{username}</a>
                 <ul className="navbar-nav">
                     <li className={currentPage = 'About me' ? "nav-item active" : "nav-item"}>
-                        <a className="nav-link" href="#aboutme">About Me</a>
+                        <a className="nav-link" href="#aboutme" onClick={() => handlePageChange('About Me')}>About Me</a>
                     </li>
                     <li className={currentPage = 'Portfolio' ? "nav-item active" : "nav-item"}>
-                        <a className="nav-link" href="#portfolio">Portfolio</a>
+                        <a className="nav-link" href="#portfolio" onClick={() => handlePageChange('Portfolio')}>Portfolio</a>
                     </li>
                     <li className={currentPage = 'Contact' ? "nav-item active" : "nav-item"}>
-                        <a className="nav-link" href="#contact">Contact</a>
+                        <a className="nav-link" href="#contact" onClick={() => handlePageChange('Contact')}>Contact</a>
                     </li>
                     <li className={currentPage = 'Resume' ? "nav-item active" : "nav-item"}>
-                        <a className="nav-link" href="#resume">Resume</a>
+                        <a className="nav-link" href="#resume" onClick={() => handlePageChange('Resume')}>Resume</a>
                     </li>
                 </ul>
             </div>
