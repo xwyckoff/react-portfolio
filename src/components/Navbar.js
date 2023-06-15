7a1deeb6d43b64fa63b36b07377f81a3c05c3f5c
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-06-14 22:13:43 -0400

    completed nav bar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 export default function Navbar({ currentPage, handlePageChange, username }) {
     return (
-        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark py-3">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#home" onClick={() => handlePageChange('Home')}>{username}</a>
                 <ul className="navbar-nav">
                     <li className={currentPage = 'About me' ? "nav-item active" : "nav-item"}>
                         <a className="nav-link" href="#aboutme">About Me</a>
+                    </li>
+                    <li className={currentPage = 'Portfolio' ? "nav-item active" : "nav-item"}>
+                        <a className="nav-link" href="#portfolio">Portfolio</a>
+                    </li>
+                    <li className={currentPage = 'Contact' ? "nav-item active" : "nav-item"}>
+                        <a className="nav-link" href="#contact">Contact</a>
+                    </li>
+                    <li className={currentPage = 'Resume' ? "nav-item active" : "nav-item"}>
+                        <a className="nav-link" href="#resume">Resume</a>
                     </li>
                 </ul>
             </div>
