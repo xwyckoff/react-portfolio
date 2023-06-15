596968eef32a1b4efe1a987d228e04be8c6d368e
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-06-15 00:24:24 -0400

    added portrait and content to about me page

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,3 +1,13 @@
+import portrait from '../../images/00023-1911885204-portrait of x___.png';
+import '../pages/css/AboutMe.css'
+
 export default function AboutMe() {
-    return <h1>test</h1>
+    return (
+        <div className="container-fluid p-3 text-center">
+            <h1 className="display-4">About Me</h1>
+            <img src={portrait} className="portrait rounded-circle float-left" alt="portrait"></img>
+            <hr></hr>
+            <p className="lead">As the title said, my name's Xander! I'm an aspiring full stack software developer looking to persue a career in said field.</p>
+        </div>
+    )
 }
