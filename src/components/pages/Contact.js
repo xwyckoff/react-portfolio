c71d24c0621ea544313c9f9c4844f70257aa5749
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-06-15 21:08:38 -0400

    created contact form

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,24 @@
+export default function Contact() {
+    return (
+        <div className="container">
+            <h1 className="display-4 text-center">Contact</h1>
+            <form className="row needs-validation">
+                <div className="form-group col-md-6">
+                    <label for="nameInput">Name</label>
+                    <input type="text" className="form-control" placeholder="Your name here" id="nameInput" required></input>
+                </div>
+                <div className="form-group col-md-6">
+                    <label for="emailInput">Email Address</label>
+                    <input type="email" className="form-control" placeholder="email@example.com"></input>
+                </div>
+                <div className="form-group col-md-12">
+                    <label for="textInput">Message</label>
+                    <textarea className="form-control" id="textInput" required></textarea>
+                </div>
+                <div className="col-12 py-3">
+                    <button class="btn btn-outline-dark" type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -7,7 +7,7 @@ export default function Portfolio() {
     const techBuildsDesc = "A simple ecommerce website project that utilizes MySQL, SemanticUI, Sequelize, Handlebars, and Express to allow you to create accounts as well as add and remove items from your shopping cart."
     return (
         <div className="container">
-            <h1 className="display-4">My Portfolio</h1>
+            <h1 className="display-4 text-center">My Portfolio</h1>
             <div className="row justify-content-center">
                 <div className="col-6">
                     <Card cardPic={techBuildsImage} cardTitle={"TechBuilds Ecommerce Website"} cardContent={techBuildsDesc} cardLink={"https://github.com/xwyckoff/BC-Project-Two"}/>
