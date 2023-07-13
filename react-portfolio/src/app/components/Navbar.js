3d627bf3c1c2a5385ef56414823a7a8df1fe8ce3
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-07-13 18:10:42 -0400

    cleared default next code, started on navbar

---
+++ react-portfolio/src/app/components/Navbar.js
@@ -0,0 +1,12 @@
+export default function NavBar() {
+    return (
+        <nav className="bg-gray-900">
+            <div className="max-w-7xl py-4 mx-3">
+                <div className="flex justify-between">
+                    <div>LOGO</div>
+                    <div>a</div>
+                </div>
+            </div>
+        </nav>
+    )
+}
