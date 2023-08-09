64ce8f181b3535d151366cd3c3d301f15803f4aa
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-08-08 21:46:05 -0400

    updated about me

--- react-portfolio/src/app/AboutMe/components/AboutMeCard.js
+++ react-portfolio/src/app/AboutMe/components/AboutMeCard.js
@@ -7,13 +7,13 @@ export default function AboutMeCard(props) {
                 <Image
                     className='rounded-full'
                     src="/portrait.png"
-                    width={400}
-                    height={400}
+                    width={600}
+                    height={600}
                     alt="Portrait of Xander"   
                 />
             </div>
-            <div className='text-lg'>
-                <p>Hello! My name is Xander. I'm a full stack web developer looking to start my career in the field. I'm a huge computer nerd and like to spend my free time gaming or working on my car. You can find some of the projects I've developed on this page, or on my GitHub.</p>
+            <div className='text-xl'>
+                <p>Hello! My name is Xander. I'm a full stack web developer looking to start my career in the field. I thrive in a fast-paced environment and have an excellent attention to detail. I'm also a huge computer nerd who loves to spend time gaming and working on my car. You can find some of the projects I've developed by myself and with a team on this page as well as my GitHub.</p>
                 <br/>
                 <p>Feel free to contact me through the social media provided on here, or through my email. Thank you for taking a look at my portfolio and I look forward to hearing from you!</p>
             </div>
