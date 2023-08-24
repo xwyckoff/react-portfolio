5ad5328db18e70960d4c87ae954df407b228ec89
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-08-23 22:24:57 -0400

    adjusted size and spacing of cards

--- react-portfolio/src/app/Portfolio/page.js
+++ react-portfolio/src/app/Portfolio/page.js
@@ -3,10 +3,11 @@ import ProjectCard from './components/ProjectCard';
 export default function Portfolio() {
     return (
         <main className='flex justify-center'>
-            <div className='grid sm:grid-cols-1 lg:grid-cols-2 gap-5'>
-                <ProjectCard imgSrc="https://www.picsum.photos/384/234" cardTitle="TEST" cardDesc="idk"/>
-                <ProjectCard imgSrc="https://www.picsum.photos/384/234" cardTitle="TEST" cardDesc="idk"/>
-                
+            <div className='grid sm:grid-cols-1 lg:grid-cols-2 gap-10'>
+                <ProjectCard imgSrc="https://www.picsum.photos/700/234" cardTitle="TEST" cardDesc="idk"/>
+                <ProjectCard imgSrc="https://www.picsum.photos/700/234" cardTitle="TEST" cardDesc="idk"/>
+                <ProjectCard imgSrc="https://www.picsum.photos/700/234" cardTitle="TEST" cardDesc="idk"/>
+                <ProjectCard imgSrc="https://www.picsum.photos/700/234" cardTitle="TEST" cardDesc="idk"/>
             </div>
         </main>
     )
