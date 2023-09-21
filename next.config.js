6171f7f6a18bed876ec4e6c5fb1213a7b7dcbf03
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-09-21 19:17:33 -0400

    testing

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const isProd = (process.env.NODE_ENV || 'production') === 'production'
 const nextConfig = {
     output: 'export',
     basePath: isProd ? "/react-portfolio" : "",
-    images: { unoptimized: true }
+    images: { unoptimized: isProd ? true : false }
 }
 
 module.exports = nextConfig
