aa80891f596da2e35996c38c28462b35b8d96c3b
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-09-21 21:15:02 -0400

    fixed padding so things wouldn't go above the nav bar

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        <body className={`bg-brand-primary text-brand-text h-screen ${adventpro.className} `}>
+        <body className={`bg-brand-primary text-brand-text h-screen pt-20 ${adventpro.className} `}>
             <Navbar />
             <div className='fixed top-1/4 right-2/4 mx-auto max-w-xs md:max-w-xl lg:max-w-3xl xl:max-w-5xl z-[-10]'>
               <BackgroundOrbs />
